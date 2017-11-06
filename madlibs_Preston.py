1b2f90b0ca326d144844e3c2e71b0e9b3fe114c7
pmerritt2003 <33422447+pmerritt2003@users.noreply.github.com>
2017-11-06 08:39:18 -0500

    initial commit

---
+++ madlibs_Preston.py
@@ -0,0 +1,25 @@
+print("Write a noun")
+noun1 = input()
+
+print("Write another noun")
+noun2 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write a verb")
+verb1 = input()
+
+
+### MAD LIB ###
+
+print(" Once upon a time, there were three " + adjective +
+"pigs. One day, their mother said, 'You are all grown up and must " + verbe +
+"' on your own.' So they left to " + verbe +
+" their houses. The first little pig wanted onlt to " + verbe + " and " + verbe+
+" all day so he " + past tense verbe + "his house with " + plural noun +
+". Thethird " + adjective + " pg knew the wold lived nearby and worked hard to " + verb +
+      " his house out of " + plural noun + ". One day, the world knocked on the first pigs "
+      + noun + "'Let me in or I'll blow your house down! The pigs didnt so the wolf "
+      + past tense verbe + "down the " + noun + " The wolf knocked on the second pig's "
+      + noun. + " 'Let me in or I'll blow your down!")
