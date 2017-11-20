a55e15d9f4be0ece0bbedaed36e16c0a18f9d373
ashumway2019 <ashumway@gcds.net>
2017-11-20 08:45:51 -0500

    Added variables and import time/Fixed errors

--- madlibs_Preston.py
+++ madlibs_Preston.py
@@ -1,8 +1,10 @@
-print("Write a noun")
-noun1 = input()
+import time
 
-print("Write another noun")
-noun2 = input()
+print("Write a plural noun")
+pluralnoun = input()
+
+print("Write a plural noun")
+pluralnoun1 = input()
 
 print("Write an adjective")
 adjective1 = input()
@@ -10,16 +12,47 @@ adjective1 = input()
 print("Write a verb")
 verb1 = input()
 
+print("Write a verb")
+verb2 = input()
+
+print("Write a verb")
+verb3 = input()
+
+print("Write a verb")
+verb4 = input()
+
+print("Write a verb in past tense.")
+pasttenseverb = input()
+
+print("Write an adjective")
+adjective2 = input()
+
+print("Write a plural noun")
+pluralnoun2 = input()
+
+print("Write a noun")
+noun1 = input()
+
+print("Write a noun")
+noun2 = input()
+
+print("Write a verb in past tense.")
+pasttenseverb1 = input()
+
+print("Write a noun")
+noun3 = input()
 
 ### MAD LIB ###
 
-print(" Once upon a time, there were three " + adjective +
-"pigs. One day, their mother said, 'You are all grown up and must " + verbe +
-"' on your own.' So they left to " + verbe +
-" their houses. The first little pig wanted onlt to " + verbe + " and " + verbe+
-" all day so he " + past tense verbe + "his house with " + plural noun +
-". Thethird " + adjective + " pg knew the wold lived nearby and worked hard to " + verb +
-      " his house out of " + plural noun + ". One day, the world knocked on the first pigs "
-      + noun + "'Let me in or I'll blow your house down! The pigs didnt so the wolf "
-      + past tense verbe + "down the " + noun + " The wolf knocked on the second pig's "
-      + noun. + " 'Let me in or I'll blow your down!")
+print(" Once upon a time, there were three " + adjective1 +
+"pigs. One day, their mother said, 'You are all grown up and must " + verb1 +
+"' on your own.' So they left to " + verb2 +
+" their houses. The first little pig wanted only to " + verb3 + " and " + verb4 +
+" all day so he " + pasttenseverb + "his house with " + pluralnoun1 +
+". The third " + adjective2 + " pig knew the wold lived nearby and worked hard to " + verb +
+      " his house out of " + pluralnoun2 + ". One day, the world knocked on the first pigs "
+      + noun1 + "'Let me in or I'll blow your house down! The pigs didnt so the wolf "
+      + pasttenseverb1 + "down the " + noun2 + " The wolf knocked on the second pig's "
+      + noun3 + " 'Let me in or I'll blow your down!")
+time.sleep(100)
+
